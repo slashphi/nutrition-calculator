3c28d7c3a12d880f354710ef909dd0cd7a2c2380
slashphi <michael.muenzing@gmail.com>
2026-07-03 22:06:40 +0200

    fix: formatting

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,9 +1,4 @@
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { App } from "./App";
 
 describe("App", () => {
